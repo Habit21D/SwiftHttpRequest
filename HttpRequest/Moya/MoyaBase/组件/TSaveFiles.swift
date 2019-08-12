a5386928116651c83817fd46539e6ea1b3ec77a1
jinyuannan <yuannan.jin@duodian.com>
2019-08-12 10:40:45 +0800

    update cache

--- HttpRequest/Moya/MoyaBase/组件/TSaveFiles.swift
+++ HttpRequest/Moya/MoyaBase/组件/TSaveFiles.swift
@@ -9,31 +9,33 @@
 import Foundation
 
 class TSaveFiles {
-    
-    class func save(path: String, data: Data) {
+
+    static let systemCache: URL? = FileManager.default.urls(for: .cachesDirectory, in: .userDomainMask).first?.appendingPathComponent("netCache", isDirectory: true)
+
+    class func getPath(path: String) -> URL? {
         let pathURL = handlePathUrl(path)
-        //拿到一个本地文件的URL
         let manager = FileManager.default
-        var url = manager.urls(for: .documentDirectory, in: .userDomainMask).first
-        url?.appendPathComponent("cache/")
-        if let urlStr = url?.absoluteString, manager.isExecutableFile(atPath: urlStr) == false {
+        var url = systemCache
+        if let urlStr = systemCache?.absoluteString, manager.isExecutableFile(atPath: urlStr) == false {
             try? manager.createDirectory(at: url!, withIntermediateDirectories: true, attributes: nil)
         }
         url?.appendPathComponent(pathURL)
-        do {
-            try data.write(to: url!)
-            print("保存到本地\(pathURL)")
-        } catch {
-            print("保存到本地文件失败")
+        return url
+    }
+
+    class func save(path: String, data: Data) {
+        if let url = getPath(path: path) {
+            do {
+                try data.write(to: url)
+                print("保存到本地\(url)")
+            } catch {
+                print("保存到本地文件失败")
+            }
         }
     }
     
     class func read(path: String) -> Data? {
-        let pathURL = handlePathUrl(path)
-        let manager = FileManager.default
-        var url = manager.urls(for: .documentDirectory, in: .userDomainMask).first
-        url?.appendPathComponent("cache/\(pathURL)")
-        if let dataRead = try? Data(contentsOf: url!) {
+        if let url = getPath(path: path), let dataRead = try? Data(contentsOf: url) {
              print("读取本地文件成功")
             return dataRead
         } else {
@@ -44,16 +46,15 @@ class TSaveFiles {
     
     class func clearCache() {
         // 取出cache文件夹目录 缓存文件都在这个目录下
-        let manager = FileManager.default
-        var url = manager.urls(for: .documentDirectory, in: .userDomainMask).first
-        url?.appendPathComponent("cache")
-        do {
-            try? manager.removeItem(at: url!)
+        if let url = systemCache {
+            do {
+                try? FileManager.default.removeItem(at: url)
+            }
         }
+
     }
     
     class func handlePathUrl(_ url: String) -> String {
         return url.replacingOccurrences(of: "/", with: "")
     }
 }
-
